02db83ecce720062e38739ca004028db156346ee
suncaiyu <42695008+suncaiyu@users.noreply.github.com>
2023-06-07 18:24:35 +0800

    Add files via upload

---
+++ Co_Routines/Co_Routines.cpp
@@ -0,0 +1,60 @@
+﻿// Co_Routines.cpp: 定义应用程序的入口点。
+//
+
+#include "Co_Routines.h"
+#include <chrono>
+#include <ctime>
+
+using namespace std;
+
+CoRet Guess()
+{
+    auto chrononow = std::chrono::system_clock::now();
+    //cout << chrononow << endl;
+
+    time_t t = time(nullptr);
+    struct tm *now = localtime(&t);
+    stringstream timeStr;
+
+    timeStr << now->tm_year + 1900 << "年";
+    timeStr << now->tm_mon + 1 << "月";
+    timeStr << now->tm_mday << "日 ";
+    timeStr << now->tm_hour << ":";
+    timeStr << now->tm_min << ":";
+    timeStr << now->tm_sec;
+    cout << timeStr.str() << endl;
+
+    srand(now->tm_sec);
+
+    int random = (rand() % 99) + 1;
+    Input input;
+    int g = -99;
+    while (g != random) {
+        g = co_await input;
+        cout << "corountine : You guess " << g << endl;
+        co_yield (random < g ? 1 : (random == g ? 0 : -1));
+    }
+    auto chronoend = std::chrono::system_clock::now();
+    std::chrono::duration<double> diff = chronoend - chrononow;
+    cout << "耗时 : " << diff << endl;
+    co_return random;
+}
+
+int main()
+{
+    cout << "我将会生成一个随机数，在 1 - 99之间，请猜一个数字，看对不对" << endl;
+    CoRet ret = Guess();
+    // 模拟用户输入
+    int guess = -1;
+    while (!ret.handle.done()) {
+        cout << "请输入一个数字" << endl;
+        cin >> guess;
+        ret.handle.promise().in = guess;
+        ret.handle.resume();
+        string res = (ret.handle.promise().out == 1 ? "larger" : (ret.handle.promise().out == 0 ? "same" : "smaller"));
+        cout << "main : The number you guessed is  " << res << endl;
+        ret.handle.resume();
+    }
+    cout << "main : random result is " << ret.handle.promise().result << endl;
+    return 0;
+}
---
+++ Co_Routines/Co_Routines.h
@@ -0,0 +1,55 @@
+﻿// Co_Routines.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#include <iostream>
+#include <coroutine>
+using namespace std;
+/* 一个简单的协程的例子，猜数字 */
+struct CoRet {
+    struct promise_type {
+        int in = 0;
+        int out = 0;
+        int result = 0;
+        // suspend_always 与 suspend_never always需要停止，never不需要
+
+        // 初始化
+        suspend_never initial_suspend() { return {}; }
+        // 结束
+        suspend_always final_suspend() noexcept { return {}; }
+        // 报错
+        void unhandled_exception() {}
+        // 每次返回时的返回值
+        CoRet get_return_object() {
+            return { 
+                coroutine_handle<promise_type>::from_promise(*this)
+            };
+        }
+        // 如果没有co_return关键字，你必须有return_void
+        //void return_void() noexcept {}
+        void return_value(int r) {
+            result = r;
+        }
+        suspend_always yield_value(int r) {
+            out = r;
+            return {};
+        }
+    };
+    // handle对象，继续调用resume等的
+    coroutine_handle<promise_type> handle;
+};
+
+struct Input {
+    int *pIn = nullptr;
+    // 判断遇到co_await/co_yield时候要不要停止并跳回到调用
+    bool await_ready() { return false; }
+    // 当需要停止的时候，在跳回之前需要做什么操作
+    void await_suspend(coroutine_handle<CoRet::promise_type> h) {
+        if (pIn == nullptr) {
+            pIn = &h.promise().in;
+        }
+    }
+    int await_resume() { return *pIn; }
+};
+// TODO: 在此处引用程序需要的其他标头。
